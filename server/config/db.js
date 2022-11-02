3df1a3fbde89839b3d2dec1cd9363f0fd23d6218
PiyushGoel <piyushgoel6293@gmail.com>
2022-11-02 15:23:28 -0400

    First commit

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+//password: lPYzrnUsjkYRzSXT
+
+
+
+module.exports = {
+  //"URI": "mongodb+srv://admin:lPYzrnUsjkYRzSXT@cluster0.hoqc9fu.mongodb.net/PROJECT 0?retryWrites=true&w=majority"
+  "URI": "mongodb+srv://artrymus:w1hPySDoR5eYdEQr@cluster0.v7jug7m.mongodb.net/web?retryWrites=true&w=majority"
+};
