4ebafa9343f35c7d2205c03b4308e31cd4ff0d01
PiyushGoel <piyushgoel6293@gmail.com>
2022-11-02 15:25:21 -0400

    connecting to mangodb atlas

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,6 @@
 
 
 module.exports = {
-  //"URI": "mongodb+srv://admin:lPYzrnUsjkYRzSXT@cluster0.hoqc9fu.mongodb.net/PROJECT 0?retryWrites=true&w=majority"
+
   "URI": "mongodb+srv://artrymus:w1hPySDoR5eYdEQr@cluster0.v7jug7m.mongodb.net/web?retryWrites=true&w=majority"
 };
